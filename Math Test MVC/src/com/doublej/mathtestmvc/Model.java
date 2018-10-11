320e9f9f9bcab5f535144ac644a49740902f8d29
Jared Corey Jones <doublej8765@hotmail.com>
2018-10-11 13:18:27 -0400

    Update Model.java
    Made improvements to game model.

--- Math Test MVC/src/com/doublej/mathtestmvc/Model.java
+++ Math Test MVC/src/com/doublej/mathtestmvc/Model.java
@@ -3,36 +3,40 @@ package com.doublej.mathtestmvc;
 public class Model {
 
     private int gameType;
-    int numberOfQuestions;
-    int correctAnswers = 0;
-    int wrongAnswers = 0;
-    String[] summary;
+    private String operator;
+    private int questionTotal;
+    private int questionIndex = 0;
+    private int correctAnswers = 0;
+    private int wrongAnswers = 0;
+    private int[] currentQuestion;
+    private String[] summary;
+    private boolean gameOver = false;
 
-    Model(int gameType, int numberOfQuestions) {
+    Model (int gameType, int numberOfQuestions) {
         this.gameType = gameType;
-        this.numberOfQuestions = numberOfQuestions;
+        this.questionTotal = numberOfQuestions;
+        switch (gameType) {
+            case 0:
+                operator = " + ";
+                break;
+            case 1:
+                operator = " - ";
+                break;
+            case 2:
+                operator = " X ";
+                break;
+            case 3:
+                operator = " / ";
+                break;
+        }
         summary = new String[numberOfQuestions];
     }
 
-    public boolean checkAnswer (long solution, long userInput, String question) {
-        if(solution == userInput)
-            correctAnswers++;
-        else
-            wrongAnswers++;
-
-        String result = " = " + userInput;
-
-        if(solution == userInput)
-             result = result.concat(" Correct!");
-        else
-            result = result.concat(" Wrong. Correct Answer = " + solution);
-
-        summary[(correctAnswers + wrongAnswers) - 1] = (correctAnswers + wrongAnswers) + ") " + question.concat(result);
-
-        return solution == userInput;
+    public int[] getCurrentQuestion() {
+        return currentQuestion;
     }
 
-    public int[] generateQuestion () {
+    public void generateCurrentQuestion () {
 
         int num1, num2;
 
@@ -78,11 +82,62 @@ public class Model {
                 break;
         }
 
-        return new int[] {num1, num2, answer};
+        currentQuestion = new int[] {num1, num2, answer};
+    }
+
+    public boolean checkUserAnswer (long userInput) {
+
+        String result = " = " + userInput;
+
+        if(userInput == currentQuestion[2]) {
+            correctAnswers++;
+            result = result.concat(" Correct!");
+        }
+        else {
+            wrongAnswers++;
+            result = result.concat(" Wrong. Correct Answer = " + currentQuestion[2]);
+        }
+
+        summary[(correctAnswers + wrongAnswers) - 1] = (correctAnswers + wrongAnswers) + ") " + getQuestionString().concat(result);
+
+        questionIndex++;
+
+        if(questionIndex == questionTotal)
+            gameOver = true;
+
+        return userInput == currentQuestion[2];
+    }
+
+    public int getNumberOfQuestions() {
+        return questionTotal;
+    }
+
+    public int getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public int getQuestionIndex() {
+        return questionIndex;
+    }
+
+    public int getWrongAnswers() {
+        return wrongAnswers;
+    }
+
+    public String[] getSummary() {
+        return summary;
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    public String getQuestionString () {
+        return "What is " + currentQuestion[0] + operator + currentQuestion[1] + "?";
     }
 
     public double getUserScore () {
-        return Math.round((double)correctAnswers / numberOfQuestions * 100);
+        return Math.round((double)correctAnswers / questionTotal * 100);
     }
-
 }
+
